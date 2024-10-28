fb54dac8ea1674b9136c992510a3b19f1f9eb999
ja153903 <jaimeabbariao@gmail.com>
2024-10-28 02:00:45 -0400

    updating with day4

---
+++ src/problem_solving/advent_of_code/yr2023/day4/main.py
@@ -0,0 +1,7 @@
+from problem_solving.advent_of_code.constants import AOC_BASE_PATH
+from problem_solving.advent_of_code.utils import read_data
+
+PATH_TO_FILE = f"{AOC_BASE_PATH}/yr2023/day4/data.in"
+
+if __name__ == "__main__":
+    data = read_data(PATH_TO_FILE)
